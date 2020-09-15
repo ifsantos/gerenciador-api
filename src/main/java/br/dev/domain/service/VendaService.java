334b90a33a926a6a32d29143f887e12d60261369
Ismael <fs.ismael@gmail.com>
2020-09-15 01:15:17 -0300

    fist adjusts on sell valitadion

--- src/main/java/br/dev/domain/service/VendaService.java
+++ src/main/java/br/dev/domain/service/VendaService.java
@@ -1,10 +1,12 @@
 package br.dev.domain.service;
 
-import java.util.stream.Collectors;
+import java.time.OffsetDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.dev.domain.model.Cliente;
+import br.dev.domain.model.ItemVenda;
 import br.dev.domain.model.Venda;
 import br.dev.domain.repository.ClienteRepository;
 import br.dev.domain.repository.ProdutoRepository;
@@ -19,15 +21,30 @@ public class VendaService {
 	private ProdutoRepository prodRepo;
 	
 	public Venda criar(Venda v) {
-		Cliente cliente = cliRepo.findById(v.getCliente().getId())
-				.orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+		Cliente cliente = validarCliente(v.getCliente());
 		v.setCliente(cliente);
-		v.setItens(v.getItens()
-				.stream()
-				.map(iv -> prodRepo.findById(iv.getId()).get())
-				.collect(Collectors.toList()));
+		v.setItens(validarItens(v.getItens()));
 		v.setData(OffsetDateTime.now());
 		return repo.save(v);
+	}
+
+	private Cliente validarCliente(Cliente c) {
+		return cliRepo.findById(c.getId())
+				.orElseThrow(() -> new RuntimeException("Cliente inexistente"));
+	}
+
+	private List<ItemVenda> validarItens(List<ItemVenda> itens) {
+		/*
+		 * Code Snippet about collectors. EX:
+		 * 	v.getItens() 
+		 * 		.stream()
+		 * 		.map(prodRepo.findById(iv.getId()).get()) 
+		 * 		.collect(Collectors.toList())
+		 */
+		
+		itens.forEach(i -> prodRepo.findById(i.getId())
+				.orElseThrow(() -> new RuntimeException("Produto inexistente: "+i.getId())));
+		return itens;
 	}
 
 }
